670ae936b93ef0970dc62d5bede9f1e2ae5af2ab
SourabGarg <sharmasourab502@gmail.com>
2024-08-10 23:30:04 +0530

    update

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 export default [
   {
     files: ['**/*.{jsx,js}'],
-    ignores: ['dist'],
+    // ignores: ['dist'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
